07b536c3711eac42d76871f938ad5aabf8947eed
OTschmoe <owenreilly129@gmail.com>
2025-10-22 12:46:12 -0500

    updated packages

--- src/main/main.ts
+++ src/main/main.ts
@@ -6,7 +6,6 @@ import { attachTitlebarToWindow, setupTitlebar } from 'custom-electron-titlebar/
 // whether you're running in development or production).
 declare const MAIN_WINDOW_WEBPACK_ENTRY: string;
 declare const MAIN_WINDOW_PRELOAD_WEBPACK_ENTRY: string;
-
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (require('electron-squirrel-startup')) {
     app.quit();
